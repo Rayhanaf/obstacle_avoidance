1ec193c794d1737e4a36ee048a85c9150f18d121
Rayhanaf <120548531+Rayhanaf@users.noreply.github.com>
2023-01-18 10:49:38 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let jarak = 0
+basic.showLeds(`
+    # . . . #
+    # . # . #
+    . # # # .
+    . . # . .
+    # # . # #
+    `)
+basic.forever(function () {
+    jarak = Tinybit.Ultrasonic_Car()
+    if (jarak < 15) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 80)
+        basic.pause(100)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 100)
+        basic.pause(100)
+    } else {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 100)
+    }
+})
